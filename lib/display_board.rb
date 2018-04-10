65e1cf2d96c48853aae8a51c2e6e6aa64156e986
Nicholas Liang <ssss021418@gmail.com>
2018-04-10 18:39:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,11 +5,11 @@ board = [" ", " ", " ",
          " ", " ", " "]
 
 def display_board(board)
-  puts(" #{board[1]} |  #{board[2]} | #{board[3]} ")
+  puts(" #{board[1]} | #{board[2]} | #{board[3]} ")
   puts("-----------")
-  puts(" #{board[1]} |  #{board[2]} | #{board[3]} ")
+  puts(" #{board[1]} | #{board[2]} | #{board[3]} ")
   puts("-----------")
-  puts(" #{board[1]} |  #{board[2]} | #{board[3]} ")
+  puts(" #{board[1]} | #{board[2]} | #{board[3]} ")
 end
 
 display_board(board)
