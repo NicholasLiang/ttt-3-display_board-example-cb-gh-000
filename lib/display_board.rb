83c1495bc6f5227b166233cbe1f83ef5b7304439
Nicholas Liang <ssss021418@gmail.com>
2018-04-10 18:42:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,9 +12,9 @@ def display_board_temp(board)
   puts(" #{board[1]} | #{board[2]} | #{board[3]} ")
 end
 def display_board(board)
-  puts(" #{board[1]} | #{board[2]} | #{board[3]} ")
+  puts("   |   |   ")
   puts("-----------")
-  puts(" #{board[1]} | #{board[2]} | #{board[3]} ")
+  puts("   |   |   ")
   puts("-----------")
-  puts(" #{board[1]} | #{board[2]} | #{board[3]} ")
+  puts("   |   |   ")
 end
